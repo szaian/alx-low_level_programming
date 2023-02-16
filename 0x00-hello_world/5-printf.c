5366634e951903d0acae479ee76403d5cab48fe2
szaian <bharathshakaarah@gmail.com>
2023-02-16 14:20:04 -0800

    task 5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+/**
+ * main  - A program that prints a line using the printf funtion
+ * Return: 0
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,");
+}
